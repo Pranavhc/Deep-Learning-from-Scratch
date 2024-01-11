c62c2fa8eb1e61e3f71a6300383a57431472f5db
Pranav Chandane <pranavhchandane@gmail.com>
2024-01-11 12:35:14 +0530

    expreriment with the training parameters

--- Examples/mnist.py
+++ Examples/mnist.py
@@ -10,15 +10,15 @@ from activation import Sigmoid
 from loss_fn import mse, mse_prime
 from network import train, predict
 
-
 def preprocess_data(x, y, limit):
     # reshape and normalize input data
     x = x.reshape(x.shape[0], 28 * 28, 1)
     x = x.astype("float32") / 255
-    # encode output which is a number in range [0,9] into a vector of size 10
-    # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    
+    # one-hot encoding - number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
     y = to_categorical(y)
     y = y.reshape(y.shape[0], 10, 1)
+    
     return x[:limit], y[:limit]
 
 
@@ -29,15 +29,15 @@ x_test, y_test = preprocess_data(x_test, y_test, 10)
 
 # neural network
 network = [
-    Dense(28 * 28, 40),
+    Dense(28 * 28, 80),
     Sigmoid(),
-    Dense(40, 25),
+    Dense(80, 40),
     Sigmoid(),
-    Dense(25, 10),
+    Dense(40, 10),
     Sigmoid()
 ]
 
-train(network, mse, mse_prime, x_train, y_train, epochs=200, learning_rate=0.1, verbose_interval=50)
+train(network, mse, mse_prime, x_train, y_train, epochs=400, learning_rate=0.3, verbose_interval=50)
 
 def calc_accuracy(x_test, y_test):
     correct_count = 0
