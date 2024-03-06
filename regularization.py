adb8718499aace9be87fe16d411f81d88f830ce3
Pranav Chandane <pranavhchandane@gmail.com>
2024-03-06 17:24:59 +0530

    idk why I called 'term' 'norm'?

--- regularization.py
+++ regularization.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class Regularization():
-    def __init__(self, norm: str, lambda_: float ):
+    def __init__(self, term: str, lambda_: float ):
         self.lambda_ = lambda_
-        self.norm = norm
+        self.term = term
 
     def L1(self, weights: np.ndarray) -> np.ndarray:
         return self.lambda_ * np.sign(weights)
@@ -12,7 +12,7 @@ class Regularization():
         return 2 * self.lambda_ * weights
 
     def regularize(self, weights: np.ndarray) -> np.ndarray:
-        if self.norm == "L1":
+        if self.term == "L1":
             return self.L1(weights)
         return self.L2(weights)
 
