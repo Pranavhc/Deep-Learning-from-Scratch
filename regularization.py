3e071597c1a514761db8c738dfdb2bb9ffcf4688
Pranav Chandane <pranavhchandane@gmail.com>
2024-03-08 17:16:02 +0530

    parameter name change, nothing serious

--- regularization.py
+++ regularization.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class Regularization():
-    def __init__(self, term: str, lambda_: float ):
+    def __init__(self, norm: str, lambda_: float ):
         self.lambda_ = lambda_
-        self.term = term
+        self.norm = norm
 
     def L1(self, weights: np.ndarray) -> np.ndarray:
         return self.lambda_ * np.sign(weights)
@@ -12,11 +12,11 @@ class Regularization():
         return 2 * self.lambda_ * weights
 
     def regularize(self, weights: np.ndarray) -> np.ndarray:
-        if self.term == "L1":
+        if self.norm == "L1":
             return self.L1(weights)
         return self.L2(weights)
 
-# Regularization Terms -
+# Regularization norms -
 # L1 = lamda * sum(|W|)
 # L2 = lambda * Sum(W^2)
 
