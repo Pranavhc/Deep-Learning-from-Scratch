1e333626d82df73b92c9f5bbc8c030c0eeabe622
Pranav Chandane <pranavhchandane@gmail.com>
2024-03-16 14:19:03 +0530

    -

--- nn/utils.py
+++ nn/utils.py
@@ -1,13 +1,8 @@
 import numpy as np
 from typing import Generator, Tuple
 
-<<<<<<< HEAD:nn/utils.py
-# this doesn't work yet (this function works but not with the current implementation of the network)
-# i don't know how to feed a batch to the network, i'm running into all sorts of shape errors
-=======
 # This doesn't work yet (the function works but not with the current implementation of the network)
 # I don't know how to feed a batch to the network, I'm running into all sorts of shape errors
->>>>>>> f2f50e2b9638ed611731ef463733068969586b60:utils.py
 
 class DataLoader:
     def __init__(self, X: np.ndarray, y: np.ndarray, batch_size: int, shuffle: False):
