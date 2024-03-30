5109e3b4fdc337a296e9c7a44ce14c37aa8e25e9
Pranav Chandane <pranavhchandane@gmail.com>
2024-03-30 16:44:45 +0530

    Restructured and added docstring

--- nn/regularization.py
+++ nn/regularization.py
@@ -1,27 +1,26 @@
 import numpy as np
 
 class Regularization():
+    """
+    Returns the derivative of regularization norm applied on weights.
+
+    regularization norms
+    ---
+    - L1 = lamda * sum(|W|)
+    - L2 = lambda * Sum(W^2)
+
+    returns
+    ---
+    The Derivative of either L1 or L2 norm w.r.t weights.
+    - L1 = lambda * sign(W)
+    - L2 = 2 * lambda * W
+    """
     def __init__(self, norm: str, lambda_: float ):
         if norm not in ["L1", "L2"]: raise ValueError("Invalid norm!")
         
         self.lambda_ = lambda_
         self.norm = norm
 
-    def L1(self, weights: np.ndarray) -> np.ndarray:
-        return self.lambda_ * np.sign(weights)
-    
-    def L2(self, weights: np.ndarray) -> np.ndarray:
-        return 2 * self.lambda_ * weights
-
     def __call__(self, weights: np.ndarray) -> np.ndarray:
-        if self.norm == "L1":
-            return self.L1(weights)
-        return self.L2(weights)
-
-# Regularization norms -
-# L1 = lamda * sum(|W|)
-# L2 = lambda * Sum(W^2)
-
-# Derivative w.r.t W -
-# L1 = lambda * sign(W)
-# L2 = 2 * lambda * W
+        if self.norm == "L1": return self.lambda_ * np.sign(weights)
+        elif self.norm == "L2": return 2 * self.lambda_ * weights
