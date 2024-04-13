1ea1d67cbd48bb347e1b598e5d92105987247e0a
Pranav Chandane <pranavhchandane@gmail.com>
2024-04-13 17:15:50 +0530

    updated docstring

--- nn/losses.py
+++ nn/losses.py
@@ -12,24 +12,17 @@ class Loss:
 
 # Mean Squared Error
 class MSE(Loss):
-    """Mean Squared Error Loss Function.
-    Computes the mean squared error between the true labels and the predicted ones.
-    """
-
+    """Mean Squared Error Loss Function."""
     def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        """Compute the MSE loss."""
         return float(np.mean(np.power(y_true - y_pred, 2)))
 
     def grad(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-        """Compute the gradient of the MSE loss with respect to the predicted labels."""
         return 2 * (y_pred - y_true) / np.size(y_true)
     
 
 # Binary Cross Entropy
 class BinaryCrossEntropy(Loss):
-    """Binary CrossEntropy Loss Function.
-    Computes the cross entropy between the true labels and the predicted ones.
-    """
+    """Binary CrossEntropy Loss Function."""
     def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
         return - y_true * np.log(y_pred) - (1 - y_true) * np.log(1 - y_pred)
@@ -39,6 +32,7 @@ class BinaryCrossEntropy(Loss):
         return -(y_true / y_pred) + (1 - y_true) / (1 - y_pred) 
     
 class CategoricalCrossEntropy(Loss):
+    """Categorical CrossEntropy Loss Function."""
     def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
         return float(-np.mean(y_true * np.log(y_pred)))
